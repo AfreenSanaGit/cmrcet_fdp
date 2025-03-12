b80221cbca5b9313d650fb5813430b10012c4b43
AfreenSanaGit <sanaafreen775@gmail.com>
2025-03-12 15:38:11 +0530

    Create groq_basic.py

---
+++ groq_basic.py
@@ -0,0 +1,24 @@
+pip install groq
+# to install groq we use above code
+
+import os
+from groq import Groq
+
+def ask_question(api_key):
+    """Ask a straightforward question to the Groq API."""
+    client = Groq(api_key=api_key)
+    question = "What are the best website to learn Python"
+    response = client.chat.completions.create(
+        messages=[{"role": "user", "content": question}],
+        model="mixtral-8x7b-32768"
+    )
+    
+    print("Question:", question)
+    print("\nResponse:", response.choices[0].message.content)
+
+def main():
+    api_key = "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"
+    ask_question(api_key)
+
+if __name__ == "__main__":
+    main()
